6818162efcd271d2115a8dd2ea65607204cda30f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-20 20:21:12 -0300

    adding megaUnaV2

--- src/br/com/fiap3espv/MegaUnaV2.java
+++ src/br/com/fiap3espv/MegaUnaV2.java
@@ -2,30 +2,44 @@ package br.com.fiap3espv;
 
 import java.util.Scanner;
 
-public class MegaUnaV1 {
+public class MegaUnaV2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int numEscolhido;
-        int numSorteado = (int) (Math.random() * 100 + 1);
-        String resposta = "SIM";
-        int tentativas = 5;
-        String jogador;
+        Scanner scan = new Scanner(System.in);
 
+        apresentarSaudacao();
+        String gamer = coletarNome(scan);
+        apresentarDesafio(gamer);
+        jogar(gamer, scan);
+        finalizarJogo();
+    }
+
+    public static void apresentarSaudacao() {
         System.out.println("Bem vindo ao Mega Una!");
+    }
 
-        System.out.println("Qual é o seu nome? ");
-        jogador = sc.next();
+    public static String coletarNome(Scanner sc) {
+        System.out.println("Qual é o seu nome?");
+        String jogador = sc.next();
         sc.nextLine();
+        return jogador;
+    }
 
+    public static void apresentarDesafio(String jogador) {
         System.out.println("Olá, " + jogador + ", nesse jogo, um número será sorteado. Você é capaz de descobrir?");
+    }
 
+    public static void jogar(String jogador, Scanner sc) {
+        int tentativas = 5;
+        int numEscolhido;
+        int numSorteado = 49;
+        String resposta = "SIM";
         do {
             tentativas--;
 
             System.out.println("Digite um número entre 1 e 100: ");
             numEscolhido = sc.nextInt();
             if(numEscolhido == numSorteado) {
-                System.out.println("Parabéns," + jogador + ", você ganhou!");
+                System.out.println("Parabéns, " + jogador + ", você ganhou!");
                 break;
             } else {
                 System.out.println("Que pena, " + jogador + "! Não foi dessa vez.");
@@ -36,7 +50,9 @@ public class MegaUnaV1 {
                 resposta = sc.next();
             }
         } while(resposta.equalsIgnoreCase("sim") && tentativas > 0);
+    }
 
+    public static void finalizarJogo() {
         System.out.println("Fim de jogo!");
     }
 }
