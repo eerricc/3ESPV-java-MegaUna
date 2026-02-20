3fecd6f3f12935dad2034c12cdd1d9b5b17a2e4e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-20 20:14:25 -0300

    mega una v1

---
+++ src/br/com/fiap3espv/MegaUnaV1.java
@@ -0,0 +1,42 @@
+package br.com.fiap3espv;
+
+import java.util.Scanner;
+
+public class MegaUnaV1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numEscolhido;
+        int numSorteado = (int) (Math.random() * 100 + 1);
+        String resposta = "SIM";
+        int tentativas = 5;
+        String jogador;
+
+        System.out.println("Bem vindo ao Mega Una!");
+
+        System.out.println("Qual é o seu nome?");
+        jogador = sc.next();
+        sc.nextLine();
+
+        System.out.println("Olá, " + jogador + ", nesse jogo, um número será sorteado. Você é capaz de descobrir?");
+
+        do {
+            tentativas--;
+
+            System.out.println("Digite um número entre 1 e 100: ");
+            numEscolhido = sc.nextInt();
+            if(numEscolhido == numSorteado) {
+                System.out.println("Parabéns," + jogador + ", você ganhou!");
+                break;
+            } else {
+                System.out.println("Que pena, " + jogador + "! Não foi dessa vez.");
+            }
+
+            if(tentativas > 0) {
+                System.out.println("Gostaria de continuar tentando? (sim / não)");
+                resposta = sc.next();
+            }
+        } while(resposta.equalsIgnoreCase("sim") && tentativas > 0);
+
+        System.out.println("Fim de jogo!");
+    }
+}
